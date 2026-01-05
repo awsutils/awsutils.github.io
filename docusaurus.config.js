749db6d5da5fbe7a08bcda433105218710b6a1d4
Vinay Bhandari <252990298+vinayb486@users.noreply.github.com>
2026-01-05 08:53:33 +0000

    Add Github Actions workflow for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'awsutils', // Usually your GitHub org/user name.
-  projectName: 'awsutils', // Usually your repo name.
+  projectName: 'awsutils.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
